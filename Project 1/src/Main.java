378f6a2042eff88217eb51b2507a06344c15bd04
RebantaGupta <80270244+RebantaGupta@users.noreply.github.com>
2023-09-11 19:12:25 -0700

    Problem 1

---
+++ Project 1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String[] args){
+        int x = 113;
+        double y = 2.71828;
+        String name = "Computer Science";
+
+        System.out.println("This is room # " + x);
+        System.out.println("e is close to " + y);
+        System.out.println("I am learning a bit about " + name);
+    }
+}
