ea7468762191d107a264815ae2138ca2f35515a0
RebantaGupta <80270244+RebantaGupta@users.noreply.github.com>
2023-09-11 19:13:18 -0700

    Problem 2

---
+++ Project 2/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main (String[] args) {
+        String eng = "English III";
+        String precalc = "Precalculus";
+        String music = "Music Theory";
+        String biotech = "Biotechnology";
+        String tech = "Principals of Technology I";
+        String latin = "Latin II";
+        String ap = "AP US History";
+        String info = "Business Computer Information Systems";
+
+        String lapan = "Ms. Lapan";
+        String gideon = "Mrs. Gideon";
+        String davis = "Mr. Davis";
+        String palmar = "Ms. Palmar";
+        String garcia = "Ms. Garcia";
+        String barnett = "Mrs. Barnett";
+        String johan = "Ms. Johannessen";
+        String james = "Mr. James";
+
+        System.out.println("+-------------------------------------------------------------+");
+        System.out.println("| 1 |" + "                           " + eng + " | " + "      " + lapan + " |");
+        System.out.println("| 2 |" + "                           " + precalc + " | " + "    " + gideon + " |");
+        System.out.println("| 3 |" + "                          " + music + " | " + "      " + davis + " |");
+        System.out.println("| 4 |" + "                         " + biotech + " | " + "     " + palmar + " |");
+        System.out.println("| 5 |" + "            " + tech + " | " + "     " + garcia + " |");
+        System.out.println("| 6 |" + "                              " + latin + " | " + "   " + barnett + " |");
+        System.out.println("| 7 |" + "                         " + ap + " | " + johan + " |");
+        System.out.println("| 8 |" + " " + info + " | " + "      " + james + " |");
+        System.out.println("+-------------------------------------------------------------+");
+
+    }
+}
