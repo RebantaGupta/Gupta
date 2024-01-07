a401b97516233408f263c073ae193d974d8ed8b7
RebantaGupta <80270244+RebantaGupta@users.noreply.github.com>
2024-01-06 17:46:52 -0800

    Ceasar Cipher

---
+++ Module 2.5 Assignment/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        CaesarCipher cipher = new CaesarCipher(10);
+
+        System.out.println("Please ENTER Prompt:");
+        System.out.println("(Please Note: Use Underscore '_' instead of Space ' ' for a Sentence)");
+
+        Scanner OriginalMessage = new Scanner(System.in);
+        String originalMessage = OriginalMessage.next();
+
+        String encryptedMessage = cipher.encrypt(originalMessage);
+        String decryptedMessage = cipher.decrypt(encryptedMessage);
+
+
+        System.out.println("Original Message: " + originalMessage);
+        System.out.println("Encrypted Message: " + encryptedMessage);
+        System.out.println("Decrypted Message: " + decryptedMessage);
+    }
+}
