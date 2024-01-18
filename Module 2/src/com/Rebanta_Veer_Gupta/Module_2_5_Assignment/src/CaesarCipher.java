d3430af2ece645973a1b73ecf91fd6ffaa380d37
RebantaGupta <80270244+RebantaGupta@users.noreply.github.com>
2024-01-18 09:45:57 -0800

    Update CaesarCipher.java

--- Module 2/src/com/Rebanta_Veer_Gupta/Module_2_5_Assignment/src/CaesarCipher.java
+++ Module 2/src/com/Rebanta_Veer_Gupta/Module_2_5_Assignment/src/CaesarCipher.java
@@ -1,29 +1,46 @@
 package com.Rebanta_Veer_Gupta.Module_2_5_Assignment.src;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CaesarCipher {
 
     private final int shift;
     private final static int ALPHABET_SIZE = 26;
-    private final static char FIRST_LETTER = 'A';
+    
+    private Map<Character, Character> encryptionMap;
+    private Map<Character, Character> decryptionMap;
 
     public CaesarCipher(int shift) {
         if (shift < 1 || shift > 25) {
             throw new IllegalArgumentException("Shift must be between 1 and 25 inclusive");
         }
         this.shift = shift;
+        initializeMaps();
+    }
+
+    private void initializeMaps() {
+        encryptionMap = new HashMap<>();
+        decryptionMap = new HashMap<>();
+
+        for (char i = 'A'; i <= 'Z'; i++) {
+            char encryptedChar = (char) ((i - 'A' + shift) % ALPHABET_SIZE + 'A');
+            encryptionMap.put(i, encryptedChar);
+            decryptionMap.put(encryptedChar, i);
+        }
+
+        for (char i = 'a'; i <= 'z'; i++) {
+            char encryptedChar = (char) ((i - 'a' + shift) % ALPHABET_SIZE + 'a');
+            encryptionMap.put(i, encryptedChar);
+            decryptionMap.put(encryptedChar, i);
+        }
     }
 
     public String encrypt(String message) {
         StringBuilder encryptedMessage = new StringBuilder();
 
         for (char character : message.toCharArray()) {
-            if (Character.isLetter(character)) {
-                char base = Character.isLowerCase(character) ? 'a' : 'A';
-                int shifted = (character - base + shift) % ALPHABET_SIZE;
-                encryptedMessage.append((char) (base + shifted));
-            } else {
-                encryptedMessage.append(character);
-            }
+            encryptedMessage.append(encryptionMap.getOrDefault(character, character));
         }
 
         return encryptedMessage.toString();
@@ -33,13 +50,7 @@ public class CaesarCipher {
         StringBuilder decryptedMessage = new StringBuilder();
 
         for (char character : encryptedMessage.toCharArray()) {
-            if (Character.isLetter(character)) {
-                char base = Character.isLowerCase(character) ? 'a' : 'A';
-                int shifted = (character - base - shift + ALPHABET_SIZE) % ALPHABET_SIZE;
-                decryptedMessage.append((char) (base + shifted));
-            } else {
-                decryptedMessage.append(character);
-            }
+            decryptedMessage.append(decryptionMap.getOrDefault(character, character));
         }
 
         return decryptedMessage.toString();
